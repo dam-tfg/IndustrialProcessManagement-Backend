e441fb83b0bdf3a7459e341a6942d8149b6ea06e
AlbertoGonzalezProg <58387793+AlbertoGonzalezProg@users.noreply.github.com>
2022-04-22 16:59:27 +0200

    Solución de bug: Faltaba por añadir la anotación @Configuration por lo que no permitía los request por XMLHttpRequest.

--- IndustrialProcessManagement/src/main/java/es/damtfg/IndustrialProcessManagement/config/WebMvcConfig.java
+++ IndustrialProcessManagement/src/main/java/es/damtfg/IndustrialProcessManagement/config/WebMvcConfig.java
@@ -3,6 +3,7 @@
  */
 package es.damtfg.IndustrialProcessManagement.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author Alberto González
  *
  */
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final long MAX_AGE_SECS = 3600;

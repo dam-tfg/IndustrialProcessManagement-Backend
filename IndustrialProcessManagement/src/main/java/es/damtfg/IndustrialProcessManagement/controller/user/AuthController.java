5b77ac648c7a2e8bd241b813e78a6afe501ae670
AlbertoGonzalezProg <58387793+AlbertoGonzalezProg@users.noreply.github.com>
2022-05-14 19:38:38 +0200

    Nuevo método para verificar si el usuario actual tiene un permiso.

--- IndustrialProcessManagement/src/main/java/es/damtfg/IndustrialProcessManagement/controller/user/AuthController.java
+++ IndustrialProcessManagement/src/main/java/es/damtfg/IndustrialProcessManagement/controller/user/AuthController.java
@@ -11,15 +11,24 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.damtfg.IndustrialProcessManagement.exception.AppException;
+import es.damtfg.IndustrialProcessManagement.model.user.security.Role;
 import es.damtfg.IndustrialProcessManagement.payload.security.JwtAuthenticationResponse;
 import es.damtfg.IndustrialProcessManagement.payload.user.LoginRequest;
+import es.damtfg.IndustrialProcessManagement.repository.user.security.UserPrincipal;
+import es.damtfg.IndustrialProcessManagement.security.CurrentUser;
 import es.damtfg.IndustrialProcessManagement.security.JwtTokenProvider;
+import es.damtfg.IndustrialProcessManagement.service.user.security.RoleServiceImpl;
+import es.damtfg.IndustrialProcessManagement.util.AppMessages;
 import es.damtfg.IndustrialProcessManagement.util.constants.ApiPath;
 
 /**
@@ -35,6 +44,9 @@ public class AuthController {
 	
 	@Autowired
 	private JwtTokenProvider tokenProvider; 
+	
+	@Autowired
+	private RoleServiceImpl roleService;
 	
 	@PostMapping("login")
 	@PreAuthorize("permitAll()")
@@ -53,6 +65,22 @@ public class AuthController {
 		String jwt = tokenProvider.generateToken(authentication);
 						
 		return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+	}
+	
+	@GetMapping("check-authorization/{roleName}")
+	@PreAuthorize("isAuthenticated()")
+	public ResponseEntity<Boolean> isAuthorized(@CurrentUser UserPrincipal currentUser, @Valid @PathVariable String roleName) {
+		
+		roleName = roleName.toUpperCase();
+		
+		roleName = (roleName.contains("ROLE_")) ? roleName : "ROLE_" + roleName;
+		
+		Role role = roleService.findByName(roleName)
+				.orElseThrow(() -> new AppException(AppMessages.EXCEPTION_USER_ROLE_NOT_SET));
+		
+		if(currentUser.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_SUPER"))) return ResponseEntity.ok(true);
+		
+		return ResponseEntity.ok(currentUser.getAuthorities().contains(new SimpleGrantedAuthority(role.getName())));
 	}
 		
 }
